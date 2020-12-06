c9c8e77365eb76e31ca25e9c110ca8d0a80a97a1
DCA-28 <danielcorcinodealbuquerque@gmail.com>
2020-12-06 19:09:40 -0300

    Laboratorio1

---
+++ cmake-build-debug/CMakeFiles/Lab1_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab1_2.dir/main.c.o"
+  "Lab1_2.exe"
+  "Lab1_2.pdb"
+  "libLab1_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab1_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
